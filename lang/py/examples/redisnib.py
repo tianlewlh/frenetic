f4b04982dc717e73925cef7e2d8a546d22461a97
Karla Saur <ksaur@umd.edu>
2014-11-06 15:33:58 -0500

    adding some port support

--- lang/py/examples/redisnib.py
+++ lang/py/examples/redisnib.py
@@ -28,7 +28,6 @@ class RedisNib:
             sys.exit(-1)
         self.r.flushall() # wipe previous entries from database
 
-    # TODO (ks) : add a set of ports for each host.
     def add_node(self, n, attr_dict=None, **attr):
         """Add a single node n and update node attributes.
 
@@ -53,10 +52,8 @@ class RedisNib:
               hash entry (which is basically a python dictionary) with the
               prefix 'nodeattr'+n (one hash entry per node attributes
         """
-        # If no attributes, just store the node
-        if not attr:
-            self.r.sadd('nodes', n)
-        else:
+        # If attributes, store them in a redis hash as ('nodeattr:'+n)
+        if attr:
             # From digraph.py
             if attr_dict is None:
                 attr_dict=attr
@@ -66,7 +63,52 @@ class RedisNib:
                 print 'The attr_dict argument must be a dictionary.'
             # Store the attributes in addition to storing the node
             self.r.hmset('nodeattr:'+n, attr_dict)
-            self.r.sadd('nodes', n)
+        # Add n to the set of nodes.
+        self.r.sadd('nodes', n)
+
+    def add_port(self, n, p):
+        """
+        Add port p to switch sw.
+        Note that adding a port to a set creates it.  (So no need to
+        call "nib.node[n]['ports'] = set()".)
+
+        TODO not tested:
+        >>> nib.add_port(event['switch_id'], event['port_id'])
+        >>> # Replaces (hopefully, not tested!!):
+        >>> # nib.node[event['switch_id']]['ports'].add(event['port_id'])
+           
+        How redis is used here:
+          Each node 'n' has a set of ports stored as a redis set indexed
+          by 'nodeports:'+n
+           
+        """
+        # First make sure the node exists
+        s = self.r.smembers('nodes') # return the set of all nodes
+        if n not in s:
+            return None
+        return self.r.sadd('nodeports:'+n, p)
+
+    def del_port(self, n, p):
+        """ Deletes port p from switch n """
+        # First make sure the node exists
+        s = self.r.smembers('nodes') # return the set of all nodes
+        if n not in s:
+            return None
+        return self.r.srem('nodeports:'+n, p)
+
+    def node_ports(self, n):
+        """ 
+        TODO/WARNING: Returns a set of _strings_ not ints.  Which is preferred?
+
+        Return the set of all ports (as strings) for node n, or empty set 
+        ([]) if no ports found.
+ 
+        Example: 
+        >>> for port in nib.node_ports(node):
+        >>> #replaces "for port in nib.node[node]['ports']:"
+ 
+        """ 
+        return self.r.smembers('nodeports:'+n)
 
     def node(self, n, attr_lookup=None):
         """ Gets node n with optional attributes.
@@ -79,8 +121,8 @@ class RedisNib:
 
             Returns None if unable to find node, or unable to find node
             with requested attribute """
-        s = self.r.smembers('nodes') # return the set of all nodes
         # First make sure the node exists
+        s = self.r.smembers('nodes') # return the set of all nodes
         if n not in s:
             return None
         else:
@@ -109,14 +151,12 @@ class RedisNib:
     # nib.edges(data=True)
     # nib.node.has_key(sw)
     # nib.add_edge(sw,host_id,outport=pt,inport=0)
-    # nib.node[sw]['ports'] = set()  # add additional data structure
-    # nib.node[event['switch_id']]['ports'].add(event['port_id'])
 
 
 ########## Tests ##################
 
 def test_add_del(nib):
-    #setup
+    # setup
     nib.r.flushall() # wipe previous
     nib.add_node('sw1',device='switch')
     nib.add_node('sw2')
@@ -135,7 +175,7 @@ def test_add_del(nib):
     assert (nib.r.exists('nodeattr:sw1') is False), 'Failed to del sw2 attrib'
 
 def test_get_node(nib):
-    #setup
+    # setup
     nib.r.flushall() # wipe previous
     nib.add_node('sw1',device='switch')
     nib.add_node('sw2')
@@ -149,10 +189,39 @@ def test_get_node(nib):
     l = ['sw1', 'sw2']
     assert ((nib.nodes() == l) is True), 'Failed to get correct set of nodes'
 
+def test_ports(nib):
+    # setup
+    nib.r.flushall() # wipe previous
+    nib.add_node('sw1',device='switch')
+
+    # test expected empty
+    assert (nib.node_ports('nodeports:sw1') == set([])),\
+        'Found unexpected port, expected empty'
+    assert (nib.r.sismember('nodeports:sw1', 80) == False), \
+        'Found unexpected port 80'
+
+    # test add_port to a legit node
+    assert (nib.add_port('sw1', 80) == 1), 'Failed add new port'
+    assert (nib.r.sismember('nodeports:sw1', 80) == True), 'Failed to add port'
+
+    # test add_port to a fake node
+    assert(nib.add_port('sw234234', 80) is None), \
+        'Failed to return None for adding port to fake switch' 
+
+    # test returning the set of nodes
+    assert (nib.node_ports('sw1') == set(['80'])),\
+        'Found unexpected set of ports for sw1'
+
+    # test deleting a port
+    assert (nib.del_port('sw1', 80) == 1), 'Failed to delete port 80'
+    assert (nib.r.sismember('nodeports:sw1', 80) == False), \
+       'Failed to delete port 80 from set sw1'
+
 def main():
     nib = RedisNib()
     test_add_del(nib)
     test_get_node(nib)
+    test_ports(nib)
     print 'All assertions pass'
 
 if __name__ == '__main__':
