a751844a96dd3106ca579a62fe95fc5096b0904f
Arjun Guha <arjun@cs.umass.edu>
2015-01-04 00:21:37 +0530

    output to dot files and compression ratio

--- lib/NetKAT_LocalCompiler.mli
+++ lib/NetKAT_LocalCompiler.mli
@@ -75,6 +75,9 @@ val equal : t -> t -> bool
 val size : t -> int
 (** [size t] returns the size of [t]. *)
 
+(* compressed_size / uncompressed_size *)
+val compression_ratio : t -> int * int
+
 val to_policy : t -> policy
 (** [to_policy t] returns a NetKAT policy that is semantically equivalent to
     [t]. If was generated from compiling a policy [p], it is not guarateed that
@@ -101,6 +104,8 @@ val eval_pipes
     third is a list of packets that are at physical locations. *)
 
 val clear_cache : unit -> unit
+
+val to_dotfile : t -> string -> unit
 
 module Field : sig
 
