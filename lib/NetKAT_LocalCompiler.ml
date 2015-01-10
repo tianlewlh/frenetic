3626eb293891960448f50505d86ece7c2213f3eb
Arjun Guha <arjun@cs.umass.edu>
2015-01-10 15:06:55 +0530

    use generalized spiros

--- lib/NetKAT_LocalCompiler.ml
+++ lib/NetKAT_LocalCompiler.ml
@@ -671,8 +671,8 @@ module Repr = struct
     | Or (p, q) -> T.sum (of_pred p) (of_pred q)
     | Neg(q)    -> T.map_r Action.negate (of_pred q)
 
-  let disj test left right =
-    if Action.Par.is_empty test then right else left
+  let disj left right =
+    if Action.Par.is_empty left then right else left
 
   let rec of_policy_k p k =
     let open NetKAT_Types in
@@ -708,7 +708,7 @@ module Repr = struct
     | DisjointUnion (p, q) ->
       of_policy_k p (fun u ->
         of_policy_k q (fun v ->
-          k (T.apply3 disj u u v)))
+          k (T.sum_generalized disj u v)))
 
 
   let rec of_policy p = of_policy_k p ident
